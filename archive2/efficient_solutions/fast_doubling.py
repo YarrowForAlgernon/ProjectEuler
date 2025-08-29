821cba774ae3444de8f37eb7f7b8f2d676868f17
YarrowForAlgernon <19341942@brookes.ac.uk>
2025-08-29 13:31:03 -0400

    Updated Solutions

--- archive2/efficient_solutions/fast_doubling.py
+++ archive2/efficient_solutions/fast_doubling.py
@@ -1,68 +1,53 @@
-from math import log
 from time import time_ns
-import sys
 
 
-
-def calculateBinaryForm(number: int) -> list:
+def convertIntegerToBase2(Integer: int) -> list:
     '''Calculates the binary form of an unsigned integer 
     as a list of boolean values.
     '''
-    binaryArray = []
-    while number > 0:
-        if number % 2 != 0:
-            binaryArray.append(True)
+    binaryForm = []
+    while Integer > 0:
+        if Integer % 2 != 0:
+            binaryForm.append(True)
         else:
-            binaryArray.append(False)
-        number //= 2
-    binaryArray.reverse()
-    return binaryArray
+            binaryForm.append(False)
+        Integer //= 2
+    return binaryForm
 
 
 def fastDoubling(n: int) -> int:
-    '''returns the nth term in the fibonacci sequence starting from 0. 
+    '''Returns the nth term in the fibonacci sequence starting from 0. 
     O(log2(n)).'''
-    a = 0
-    b = 1
-    nBinaryForm = calculateBinaryForm(n)
-    while nBinaryForm:
-        n = nBinaryForm[0]
-        c = a * ((2 * b) - a)
-        d = (a**2) + (b**2)
-        if not n: # if bit is 0
+    a = 0 #F0
+    b = 1 #F1
+    binaryForm = convertIntegerToBase2(n)
+    while binaryForm:
+        mostSignificantBit = binaryForm[-1]
+        c = a * ((2 * b) - a) #Identity F2n
+        d = (a**2) + (b**2) #Identity F2n+1
+        if not mostSignificantBit: # if bit is 0
             a, b = c, d
         else:
             a, b = d, c + d
-        nBinaryForm.pop(0)
+        binaryForm.pop(-1)
     return a
 
 
-def sumEvenTerms(n: int) -> float:
-    '''Only works if the value of n is a multiple of 3 and therefore 
-    a positive term. A quotient division is used to save time and
-    space in divisions involving large numbers.'''
-    isOdd = False
-    evenSum = fastDoubling(n+2) - 1
-    if evenSum % 2 != 0:
-        isOdd = True
-    evenSum //= 2 
-    if isOdd:
-        evenSum += 0.5
-    return evenSum
+def main(n: int = 33) -> float:
+    '''Returns the sum of all even Fibonacci terms under the nth term.'''
+    n = setNthTermToEvenNumber(n)
+    evenSummation = fastDoubling(n+2) - 1
+    evenSummation //= 2 
+    return evenSummation
 
 
-def main(n: int = 33) -> int:
-    '''if n isn't an even term, it will set it to the last even term, 
-    in order to calculate the sum of all previous even terms.'''
-    if n % 3 != 0:
-        n = n - (n % 3)
-    finalSum = sumEvenTerms(n)
-    return finalSum
-
+def setNthTermToEvenNumber(n: int) -> int:
+    '''Sets the value of n to a multiple of 3 to ensure it is
+    an even number.'''
+    return (n - (n % 3))
 
 if __name__ == "__main__":
-    sys.set_int_max_str_digits(0)
     startTime = time_ns()
     evenSum = main()
     stopTime = time_ns()
-    print(evenSum, "\nNumber of digits: " + str(len(str(evenSum))), "\nTime (ns): " + str(stopTime - startTime))
+    print(evenSum, "\nNumber of digits: " + str(len(str(evenSum))), "\nTime (ms): " + str((stopTime - startTime) / 1000000))
